c7fb6c7ad62ab3578e120781a1284347387d3c47
Esteban Echeverry <eecheverry@knowark.com>
2021-03-26 10:51:41 -0500

    Modify Connector to receive *args and **kwargs.

--- modelark/connector/connector.py
+++ modelark/connector/connector.py
@@ -3,8 +3,8 @@ from .connection import Connection
 
 
 class Connector(Protocol):
-    async def get(self, zone='') -> Connection:
+    async def get(self, *args, **kwargs) -> Connection:
         """Get a connection from the pool"""
 
-    async def put(self, connection: Connection, zone='') -> None:
+    async def put(self, connection: Connection, *args, **kwargs) -> None:
         """Return a connection to the pool"""
